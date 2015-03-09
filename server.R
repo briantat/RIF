86254078d8a238a442ee508808200a469fb79f3d
briantat <tatztastic@gmail.com>
2015-03-08 17:07:04 -0700

    Version1

---
+++ server.R
@@ -0,0 +1,239 @@
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+library(ggvis)
+library(ggplot2)
+library(foreign)
+library(datasets)
+
+data1 <- read.csv("data/final_data.csv")
+all_data1 <- data1
+
+shinyServer(function(input, output, session) {
+  dat <- reactive({
+    d <- all_data1
+    
+    if(input$options == "Yes"){
+      d<-d[d$alreadyInRIF == 1,]
+    }
+    if(input$options == "No"){
+      d<-d[d$alreadyInRIF == 0,]
+    }
+    #d <- as.data.frame(d)
+    d
+  })
+  
+  vis <- reactive({
+    dat %>% ggvis(~MEALS, ~PCST_E28,fill = ~aiRIFtxt) %>%
+      add_axis("x", title = "Meals") %>%
+      add_axis("y", title = "Reading Scores") %>%
+      set_options(width = 500, height = 500) %>%
+      layer_points( size :=50, size.hover:=200,
+                    fillOpacity := 0.8, fillOpacity.hover := 1, key := ~SNAME) %>%
+      add_tooltip(function(df) paste0("<b>",df$SNAME,"</b><br>(",df$MEALS,", ",df$PCST_E28,")")) %>%
+      add_legend("fill", title = "Already In RIF", values = c("Yes", "No")) %>%
+      scale_numeric("x",domain = c(0,100)) %>%   scale_numeric("y",domain = c(0,600))
+    
+  })
+  vis %>% bind_shiny("plot1")
+  
+  output$mealsPlot <- renderPlot({
+    junk <- data1
+    junk2 <- quantile(junk$MEALS, probs = c(input$Mquant/100))
+    junk$mealsQuant <- findInterval(junk$MEALS, junk2)
+    
+    a <- ggplot(junk, aes(x = MEALS, fill = factor(mealsQuant)))
+    a + geom_histogram(binwidth = 3)+labs(x = "Meals")+
+      scale_fill_manual(values = c("darkblue", "orange"))
+    
+  })
+  
+  output$PCSTPlot <- renderPlot({
+    junk <- data1
+    junk3 <- quantile(junk$PCST_E28, probs = c(input$Equant/100))
+    junk$pcst_e28Quant <- findInterval(junk$PCST_E28, junk3)
+    
+    a <- ggplot(junk, aes(x = PCST_E28, fill = factor(pcst_e28Quant)))
+    a + geom_histogram(binwidth = 10)+labs(x = "Reading Scores")+
+      scale_fill_manual(values = c("orange","darkblue"))
+  })
+  
+  # Return the requested data1set
+  groupInput <- reactive({
+    group2 <- rep(FALSE,dim(data1)[1])
+    if("A" %in% input$checkGroup){group2 <- (data1$mealsQuant == 3 & data1$pcst_e28Quant == 0)|group2}
+    if("B" %in% input$checkGroup){group2 <- (data1$mealsQuant == 3 & data1$pcst_e28Quant == 1)|group2}
+    if("C" %in% input$checkGroup){group2 <- (data1$mealsQuant == 3 & data1$pcst_e28Quant == 2)|group2}
+    if("D" %in% input$checkGroup){group2 <- (data1$mealsQuant == 3 & data1$pcst_e28Quant == 3)|group2}
+    if("E" %in% input$checkGroup){group2 <- (data1$mealsQuant == 2 & data1$pcst_e28Quant == 0)|group2}
+    if("F" %in% input$checkGroup){group2 <- (data1$mealsQuant == 2 & data1$pcst_e28Quant == 1)|group2}
+    if("G" %in% input$checkGroup){group2 <- (data1$mealsQuant == 2 & data1$pcst_e28Quant == 2)|group2}
+    if("H" %in% input$checkGroup){group2 <- (data1$mealsQuant == 2 & data1$pcst_e28Quant == 3)|group2}
+    if("I" %in% input$checkGroup){group2 <- (data1$mealsQuant == 1 & data1$pcst_e28Quant == 0)|group2}
+    if("J" %in% input$checkGroup){group2 <- (data1$mealsQuant == 1 & data1$pcst_e28Quant == 1)|group2}
+    if("K" %in% input$checkGroup){group2 <- (data1$mealsQuant == 1 & data1$pcst_e28Quant == 2)|group2}
+    if("L" %in% input$checkGroup){group2 <- (data1$mealsQuant == 1 & data1$pcst_e28Quant == 3)|group2}
+    if("M" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 0)|group2}
+    if("N" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 1)|group2}
+    if("O" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 2)|group2}
+    if("P" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 3)|group2}
+    
+    #if("A" %in% input$checkGroup){group2 <- (data1$mealsQuant == 3 & data1$pcst_e28Quant == 0)|group2}
+    
+    #if("B" %in% input$checkGroup){group2 <- (data1$mealsQuant == 2 & data1$pcst_e28Quant == 0) |
+    #                         (data1$mealsQuant == 3 & data1$pcst_e28Quant == 1)|group2}
+    
+    #if("C" %in% input$checkGroup){group2 <- (data1$mealsQuant == 1 & data1$pcst_e28Quant == 0) |
+    #                         (data1$mealsQuant == 2 & data1$pcst_e28Quant == 1) |
+    #                         (data1$mealsQuant == 3 & data1$pcst_e28Quant == 2)|group2}
+    
+    #if("D" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 0) |
+    #                         (data1$mealsQuant == 1 & data1$pcst_e28Quant == 1) |
+    #                         (data1$mealsQuant == 2 & data1$pcst_e28Quant == 2) |
+    #                         (data1$mealsQuant == 3 & data1$pcst_e28Quant == 3)|group2}
+    
+    #if("E" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 1) |
+    #                         (data1$mealsQuant == 1 & data1$pcst_e28Quant == 2) |
+    #                         (data1$mealsQuant == 2 & data1$pcst_e28Quant == 3)|group2}
+    
+    
+    #if("F" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 2) |
+    #                         (data1$mealsQuant == 1 & data1$pcst_e28Quant == 3)|group2}
+    
+    #if("G" %in% input$checkGroup){group2 <- (data1$mealsQuant == 0 & data1$pcst_e28Quant == 3)|group2}
+    
+    newdata1 <- data1[group2,c(1,8,13,18)]
+    
+    if(input$sort == "School Name"){newdata1 <- newdata1[order(newdata1$SNAME),]}
+    
+    if(input$sort == "Meals"){newdata1 <- newdata1[order(-newdata1$MEALS,newdata1$PCST_E28),]}
+    
+    if(input$sort == "Reading Scores"){newdata1 <- newdata1[order(newdata1$PCST_E28),]}
+    
+    newdata1
+    
+  })
+  
+  output$value <- renderPrint({ input$checkGroup })
+  
+  
+  output$groupInput <- renderTable({
+    groupInput()
+  })
+  
+  ## Interactive Map ###########################################
+  
+  # Create the map
+  map <- createLeafletMap(session, "map")
+  
+
+  zipsInBounds <- reactive({
+    if (is.null(input$map_bounds))
+      return(data1[FALSE,])
+    bounds <- input$map_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    group3 <- c()
+    if("A" %in% input$checkGroup){group3 <- c(group3,"A")}
+    if("B" %in% input$checkGroup){group3 <- c(group3,"B")}
+    if("C" %in% input$checkGroup){group3 <- c(group3,"C")}
+    if("D" %in% input$checkGroup){group3 <- c(group3,"D")}
+    if("E" %in% input$checkGroup){group3 <- c(group3,"E")}
+    if("F" %in% input$checkGroup){group3 <- c(group3,"F")}
+    if("G" %in% input$checkGroup){group3 <- c(group3,"G")}
+    if("H" %in% input$checkGroup){group3 <- c(group3,"H")}
+    if("I" %in% input$checkGroup){group3 <- c(group3,"I")}
+    if("J" %in% input$checkGroup){group3 <- c(group3,"J")}
+    if("K" %in% input$checkGroup){group3 <- c(group3,"K")}
+    if("L" %in% input$checkGroup){group3 <- c(group3,"L")}
+    if("M" %in% input$checkGroup){group3 <- c(group3,"M")}
+    if("N" %in% input$checkGroup){group3 <- c(group3,"N")}
+    if("O" %in% input$checkGroup){group3 <- c(group3,"O")}
+    if("P" %in% input$checkGroup){group3 <- c(group3,"P")}
+    
+    
+    
+    
+    
+    
+    subset(data1,
+           LAT >= latRng[1] & LAT <= latRng[2] &
+             LONG >= lngRng[1] & LONG <= lngRng[2])
+    
+  })
+  
+  locdata <- reactive({
+    group3 <- c()
+    if("A" %in% input$checkGroup){group3 <- c(group3,"A")}
+    if("B" %in% input$checkGroup){group3 <- c(group3,"B")}
+    if("C" %in% input$checkGroup){group3 <- c(group3,"C")}
+    if("D" %in% input$checkGroup){group3 <- c(group3,"D")}
+    if("E" %in% input$checkGroup){group3 <- c(group3,"E")}
+    if("F" %in% input$checkGroup){group3 <- c(group3,"F")}
+    if("G" %in% input$checkGroup){group3 <- c(group3,"G")}
+    if("H" %in% input$checkGroup){group3 <- c(group3,"H")}
+    if("I" %in% input$checkGroup){group3 <- c(group3,"I")}
+    if("J" %in% input$checkGroup){group3 <- c(group3,"J")}
+    if("K" %in% input$checkGroup){group3 <- c(group3,"K")}
+    if("L" %in% input$checkGroup){group3 <- c(group3,"L")}
+    if("M" %in% input$checkGroup){group3 <- c(group3,"M")}
+    if("N" %in% input$checkGroup){group3 <- c(group3,"N")}
+    if("O" %in% input$checkGroup){group3 <- c(group3,"O")}
+    if("P" %in% input$checkGroup){group3 <- c(group3,"P")}
+    subset(data1, letter %in% group3)
+    m <- all_data1 %>% filter(letter %in% group3)
+    m
+  })
+  
+  session$onFlushed(once=TRUE, function() {
+    paintObs <- observe({
+      #colorData <- all_data1[["letter"]]
+      #colors <- brewer.pal(16, "Spectral")[colorData]
+      #colors <- colors[match(locdata()$SNAME, all_data1$SNAME)]
+      
+      map$clearShapes()
+      map$addCircle(locdata()$LAT,locdata()$LONG,100)#,
+       #             options = list(color = colors))
+    })
+    
+    session$onSessionEnded(paintObs$suspend)
+  })
+  
+  # Show a popup at the given location
+  showZipcodePopup <- function(id, lat, lng) {
+    selectedZip <- locdata()[locdata()$LAT == lat & locdata()$LONG == lng,]
+    content <- as.character(tagList(
+      tags$h5("Letter:", as.character(selectedZip$letter)),
+      tags$strong(HTML(sprintf("%s",
+                               selectedZip$SNAME
+      ))), tags$br(),
+      sprintf("Reading Score: %s", as.numeric(selectedZip$PCST_E28)), tags$br(),
+      sprintf("Percent of students eligible for reduced lunch: %s%%", as.integer(selectedZip$MEALS)), tags$br(),
+      sprintf("Number of enrolled students: %s", selectedZip$ENROLL)
+    ))
+    map$showPopup(lat, lng,content)
+  }
+  
+  # When map is clicked, show a popup with city info
+  clickObs <- observe({
+    map$clearPopups()
+    event <- input$map_shape_click
+    if (is.null(event))
+      return()
+    
+    isolate({
+      showZipcodePopup(event$id, event$lat, event$lng)
+    })
+  })
+  
+  session$onSessionEnded(clickObs$suspend)
+  
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,96 @@
+library(shiny)
+library(leaflet)
+library(ggvis)
+
+shinyUI(navbarPage("Reading Is Fundamental", id="nav",
+  tabPanel("Map",
+    div(class="outer",
+      tags$head(
+        # Include our custom CSS
+        includeCSS("styles.css"),
+        includeScript("gomap.js")
+      ),
+                             
+    leafletMap("map", width="100%", height="100%",
+         initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+         initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+         options=list(
+          center = c(34.052363, -118.399373),
+           zoom = 10,
+           maxBounds = list(list(33.316168, -118.924221), list(34.860564, -117.592129))
+           
+           )
+          ),
+    
+    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                  top = 70, left = "auto", right = 20, bottom = "auto",
+                  width = 330, height = "auto"
+      ),
+                                
+       tags$div(id="cite",
+          'Data compiled for ', tags$em('Reading is Fundamental')
+       )
+     )
+    ),
+                   
+   tabPanel("Current Plot",
+      sidebarLayout(
+        sidebarPanel(
+          wellPanel(
+            selectInput("options","Schools In The RIF Program",c("All","Yes","No"))    
+              )
+            ),  
+          mainPanel(
+            ggvisOutput("plot1")     
+          )    
+        )
+      ),
+                   
+                   
+    tabPanel("Distribution of Variables", 
+    # Sidebar with a slider input for the number of bins
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("Mquant",
+            "Quantile for Meals",
+            min = 75,
+            max = 100,
+            value = 85),
+         sliderInput("Equant",
+            "Quantile for Reading Scores",
+            min = 0,
+            max = 25,
+            value = 20) 
+        ),
+     # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("mealsPlot"),#,width = "50%"),
+          plotOutput("PCSTPlot")#,width = "50%")
+        )
+      )
+    ),
+  tabPanel("Table of Reference", 
+        sidebarLayout(
+          sidebarPanel(
+            checkboxGroupInput("checkGroup", label = h3("Selection of Categories"), 
+                                choices = LETTERS[1:16],
+                                selected = LETTERS[1])
+                              ),
+          mainPanel(
+            img(src="color.PNG")                
+            )
+        )
+    ),
+  tabPanel("Lists",
+    sidebarLayout(
+       sidebarPanel(
+          selectInput("sort", "Sort by:", 
+                       choices = c("School Name","Meals","Reading Scores"))
+            ),
+
+       mainPanel(
+          h4("Lists"),
+          tableOutput(outputId="groupInput") )
+        )
+    )
+))
